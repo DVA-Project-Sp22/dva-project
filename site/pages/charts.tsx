719a19131e6e58e4b775735e08d6d4d3ba3b43b1
Ryan Maas <ryan.h.maas@gmail.com>
2022-04-03 11:40:38 -0400

    add charts

--- site/pages/charts.tsx
+++ site/pages/charts.tsx
@@ -5,23 +5,22 @@ export default function Charts(){
   const data = {
     labels: [
       'Loudness',
-      'Danceability',
-      'Attribute X',
-      'Attribute Y',
+      'Tempo',
+      'Artist Familiarity',
     ],
     datasets: [{
       label: 'Your Disliked Songs',
-      data: [0.43, 0.32, 0.74, 0.63],
+      data: [0.43, 0.32, 0.74],
       fill: true,
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgb(255, 99, 132)',
-      pointBackgroundColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(129, 140, 248, 0.2)',
+      borderColor: 'rgb(129, 140, 248)',
+      pointBackgroundColor: 'rgb(129, 140, 248)',
       pointBorderColor: '#F3F4F6',
       pointHoverBackgroundColor: '#F3F4F6',
-      pointHoverBorderColor: 'rgb(255, 99, 132)'
+      pointHoverBorderColor: 'rgb(129, 140, 248)'
     }, {
       label: 'Songs in Generated Playlist',
-      data: [0.55, 0.75, 0.89, 1.0],
+      data: [0.55, 0.75, 1.0],
       fill: true,
       backgroundColor: 'rgba(54, 162, 235, 0.2)',
       borderColor: 'rgb(54, 162, 235)',
